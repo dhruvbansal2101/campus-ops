738c10882025e786e077ac4963942a44c3f7f48d
dhruvbansal2101 <bansaldhruv1404@gmail.com>
2026-04-12 16:34:20 +0530

    INITIAL COMMIT WITH STRUCTURE MADE FOR WEBSITE

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-[#fdf6f0]">
+      <h1 className="text-2xl font-bold text-gray-800">
+        CampusOps 🚀
+      </h1>
+    </div>
+  );
+}
